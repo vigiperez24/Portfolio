84d660bd1974e25863dac34d49a7b815d139f95f
vigi perez <vigiperez2324@gmail.com>
2025-10-07 20:50:37 +0800

    Fixing bugs & clean

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,63 +15,48 @@ function AllRights() {
   const currentYear = new Date().getFullYear();
 
   // Custom SVG icon components
+
   const EnvelopeIcon = () => (
-    // Email icon
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className="w-4 h-4"
       fill="currentColor"
       viewBox="0 0 24 24"
     >
-      <path d="..." />
+      <path d="M20 4H4a2 2 0 00-2 2v12a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2zm0 2v.01L12 13 4 6.01V6h16zM4 18V8l8 7 8-7v10H4z" />
     </svg>
   );
 
   const PhoneIcon = () => (
-    // Phone icon
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className="w-4 h-4"
       fill="currentColor"
       viewBox="0 0 24 24"
     >
-      <path d="..." />
+      <path d="M6.62 10.79a15.72 15.72 0 006.59 6.59l2.2-2.2a1 1 0 011.11-.21 11.36 11.36 0 003.55.57 1 1 0 011 1V20a1 1 0 01-1 1A17 17 0 013 4a1 1 0 011-1h3.5a1 1 0 011 1 11.36 11.36 0 00.57 3.55 1 1 0 01-.21 1.11l-2.24 2.13z" />
     </svg>
   );
 
   const FacebookIcon = () => (
-    // Facebook icon
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className="w-4 h-4"
       fill="currentColor"
       viewBox="0 0 24 24"
     >
-      <path d="..." />
+      <path d="M22 12a10 10 0 10-11.5 9.95v-7.05h-2.3V12h2.3V9.8c0-2.27 1.35-3.53 3.43-3.53.99 0 2.02.18 2.02.18v2.22h-1.14c-1.13 0-1.48.7-1.48 1.42V12h2.52l-.4 2.9h-2.12v7.05A10 10 0 0022 12z" />
     </svg>
   );
 
   const GithubIcon = () => (
-    // GitHub icon
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className="w-4 h-4"
       fill="currentColor"
       viewBox="0 0 24 24"
     >
-      <path d="..." />
-    </svg>
-  );
-
-  const SendIcon = () => (
-    // Send icon (not used in JSX below)
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="w-4 h-4"
-      fill="currentColor"
-      viewBox="0 0 24 24"
-    >
-      <path d="..." />
+      <path d="M12 2C6.48 2 2 6.58 2 12.26c0 4.5 2.87 8.32 6.84 9.67.5.09.68-.22.68-.48 0-.24-.01-.87-.01-1.71-2.78.61-3.37-1.36-3.37-1.36-.45-1.16-1.11-1.47-1.11-1.47-.91-.64.07-.63.07-.63 1 .07 1.53 1.06 1.53 1.06.89 1.56 2.34 1.11 2.91.85.09-.66.35-1.11.63-1.36-2.22-.26-4.56-1.14-4.56-5.07 0-1.12.39-2.04 1.03-2.76-.1-.26-.45-1.3.1-2.7 0 0 .84-.27 2.75 1.03A9.35 9.35 0 0112 6.8a9.3 9.3 0 012.5.34c1.91-1.3 2.75-1.03 2.75-1.03.55 1.4.2 2.44.1 2.7.64.72 1.03 1.64 1.03 2.76 0 3.94-2.34 4.8-4.57 5.06.36.32.68.94.68 1.9 0 1.37-.01 2.47-.01 2.81 0 .27.18.58.69.48A10.02 10.02 0 0022 12.26C22 6.58 17.52 2 12 2z" />
     </svg>
   );
 
